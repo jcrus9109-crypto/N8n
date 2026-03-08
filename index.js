26e55025c5329f5c31227e7a15a5e6f4f80ea33d
jcrus9109-crypto <jcrus9109@gmail.com>
2026-03-08 08:40:57 +0200

    Refactor n8n startup to use child_process.spawn

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ process.env.EXECUTIONS_DATA_MAX_AGE = '1';
 process.env.N8N_GRACEFUL_SHUTDOWN_TIMEOUT = '0';
 process.env.NODE_OPTIONS = '--max-old-space-size=768';
 
-require('n8n');
+const { spawn } = require('child_process');
+
+const n8n = spawn('./node_modules/.bin/n8n', ['start'], {
+  stdio: 'inherit',
+  env: process.env
+});
+
+n8n.on('close', (code) => {
+  console.log(`n8n exited with code ${code}`);
+  process.exit(code);
+});
